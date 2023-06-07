36ea0fbcf76efd8aa7d7a00a11cd7d5b0db28df0
KSrikarC <ksrikarc@gmail.com>
2023-06-07 14:39:37 +0530

    Added more utilites

--- sql_connection.py
+++ sql_connection.py
@@ -1,6 +1,5 @@
-from Custom_Func import cls
 import mysql.connector
-cls()
+
 
 try:
     connection = mysql.connector.connect(host='localhost',
@@ -24,14 +23,33 @@ try:
     connection.commit()
 
     query = """INSERT INTO PYTHON_DB(id,name) VALUES(%s,%s)"""
-    data = (1,'Srikar')
-    cursor.execute(query,data)
+    data = ((2,'Chandu'))
+    #cursor.execute(query,data)
     print('DB updated successfully ! ')
     connection.commit()
 
+    query = f'show columns from c361cohort.{tables[0]}'
+    cursor.execute(query)
+    columns = cursor.fetchall()
+    connection.commit()
 
+    cols = []
+    for _ in columns:
+        cols.append(_[0])
+
+    for val in cols:
+        print(val,end='\t')
+    print()
+    query = 'SELECT * FROM PYTHON_DB'
+    cursor.execute(query)
+    records = cursor.fetchall()
+    
+    for _ in records:
+        print(_[0],_[1],sep='\t')
+
+    
 except Exception as e:
-    print(str(e))
+    print('Identified Error is : ',str(e))
 
 
 
