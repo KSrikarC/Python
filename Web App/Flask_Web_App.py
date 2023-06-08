ec6c3c2bf1f0be06f4ce788b61f2734fa833d8a3
KSrikarC <ksrikarc@gmail.com>
2023-06-08 11:20:02 +0530

    Initial commit

--- Web App/Flask_Web_App.py
+++ Web App/Flask_Web_App.py
@@ -1,4 +1,18 @@
-import flask
+import os
+os.system('cmd /c cls')
+
+
+from flask import Flask , render_template
+from sql_methods import *
+app = Flask(__name__)
 
 @app.route('/')
-def 
+def index():
+    records = fetch_records(start_connection())
+    return render_template("index.html",records=records)
+
+
+
+
+if __name__ == '__main__':
+    app.run()
